006e0cefdfc2e369008c529f306dfa41228f6266
Sergii <zx9289@gmail.com>
2025-03-03 19:49:48 +0200

    Day 2 connect to ChatGPT + updated dialog.mode

--- bot.py
+++ bot.py
@@ -6,11 +6,17 @@ from gpt import *
 from util import *
 
 load_dotenv(dotenv_path='config.env')
-TOKEN = os.getenv('TG_TOKEN')
+TG_TOKEN = os.getenv('TG_TOKEN')
 AI_TOKEN = os.getenv('AI_TOKEN')
+
+dialog = Dialog()  # створюємо екземпляр порожнього класу Dialog
+dialog.mode = 'main'  # Додаємо новий атрибут mode до об'єкту dialog та призначаємо йому значення 'main' as default
+
+chatgpt = ChatGptService(token=AI_TOKEN)
 
 
 async def start(update, context):
+    dialog.mode = 'main'
     msg = load_message('main')  # завантажуємо вміст файлу main у змінну msg
     await send_photo(update, context, name='main')
     await send_text(update, context, msg)  # виводимо текст
@@ -34,22 +40,19 @@ async def gpt_dialog(update, context):
     text = update.message.text  # отримуємо текст із запросу (те, що пишуть в командній строці боту)
     prompt = load_prompt('gpt')  # підтягуємо вміст файлу /prompts/gpt.txt для створення промпту
     # prompt - це коротка інструкція для запиту штучному інтелекту.
-    answer = await chatgtp.send_question(prompt_text=prompt, message_text=text)  # зберігаємо відповідь ШІ у змінну
+    answer = await chatgpt.send_question(prompt_text=prompt, message_text=text)  # зберігаємо відповідь ШІ у змінну
     await send_text(update, context, answer)
 
 
 async def hello(update, context):
     if dialog.mode == 'gpt':
         await gpt_dialog(update, context)
+    elif dialog.mode == 'main':
+        await send_text(update, context, 'Hello ' + update.message.text)
 
-
-dialog = Dialog()  # створюємо екземпляр порожнього класу Dialog
-dialog.mode = None  # Додаємо новий атрибут mode до об'єкту dialog та призначаємо йому значення None
-
-chatgtp = ChatGptService(token=AI_TOKEN)
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token(TOKEN).build()
+    app = ApplicationBuilder().token(TG_TOKEN).build()
     app.add_handler(CommandHandler('start', start))  # обробник команди /start
     app.add_handler(CommandHandler('gpt', gpt))  # обробник команди /gpt
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))  # обробник
